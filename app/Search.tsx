dd0dc73bfaa26d26bae91c26ae67c398dcedf6f8
Nikhil Saraf <nsaraf98@gmail.com>
2023-04-02 02:52:13 +0530

    add use router hook

--- app/Search.tsx
+++ app/Search.tsx
@@ -1,16 +1,18 @@
 "use client";
 
+import { useRouter } from "../modules/router/useRouter";
 import { useState, useTransition } from "react";
 
 export default function Search({ search: initialSearch }: { search: string }) {
 	const [isPending, startTransition] = useTransition();
+	const router = useRouter();
 	return (
 		<>
 			<input
 				defaultValue={initialSearch}
 				onChange={(e) => {
 					startTransition(() => {
-						window.router.replace("/?q=" + e.target.value);
+						router.replace("/?q=" + e.target.value);
 					});
 				}}
 			/>
